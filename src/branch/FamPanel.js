b6e7e23f1612a09cf27967a23ce0efcd1bb8d791
Yohei Shimomae <yoheishimomae@Yoheis-MacBook-Air.local>
2023-06-21 17:02:58 -0700

    Fixed a bug with date conversion

--- src/branch/FamPanel.js
+++ src/branch/FamPanel.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import './FamPanel.css';
 
+const months = {
+  0: 'January',
+  1: 'February',
+  2: 'March',
+  3: 'April',
+  4: 'May',
+  5: 'June',
+  6: 'July',
+  7: 'August',
+  8: 'September',
+  9: 'October',
+  10: 'November',
+  11: 'December'
+}
+
 class FamPanel extends Component {
 
   render() {
+
+    function formatDate(D) {
+      const d = new Date(D);
+      const year  = d.getUTCFullYear();
+      const date = d.getUTCDate();
+      const mon = d.getUTCMonth();
+
+      return months[mon] + ' ' + date + ', ' + year;
+    }
+
     const item = this.props.data;
 
     const nameJP = item.JAPANESE_NAME;
 
     const B = item.BORN;
-    const born = B ? (B.length > 4 ? new Date(B).toDateString().slice(4) : B) : null;
+    const born = B ? (B.length > 4 ? formatDate(B) : B) : null;
     const bornLoc = item.BIRTH_PLACE;
 
     const D = item.DEATH;
-    let death = D ? (D.length > 4 ? new Date(D).toDateString().slice(4) : D) : null;
+    let death = D ? (D.length > 4 ? formatDate(D) : D) : null;
     const deathLoc = item.DEATH_PLACE;
 
     if (born && death) {
